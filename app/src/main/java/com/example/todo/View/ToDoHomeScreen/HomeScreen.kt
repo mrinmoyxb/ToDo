6d6c10b2123f5f362b5ead759967870d822cf20a
Mrinmoy Borah <141025752+mrinmoyxb@users.noreply.github.com>
2024-01-22 14:31:29 +0530

    My Second commit. Testing

--- app/src/main/java/com/example/todo/View/ToDoHomeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/todo/View/ToDoHomeScreen/HomeScreen.kt
@@ -1,2 +1,15 @@
 package com.example.todo.View.ToDoHomeScreen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DisplayHomeScreen(){
+    HomeScreen()
+}
